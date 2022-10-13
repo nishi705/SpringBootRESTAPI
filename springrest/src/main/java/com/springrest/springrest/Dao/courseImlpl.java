0dc3e513476cf9269464b63414a065ba0e799318
Nishi Nanda Singh <n.nandasingh221@gmail.com>
2022-10-13 21:14:32 +0530

    "restapi"

--- springrest/src/main/java/com/springrest/springrest/Dao/courseImlpl.java
+++ springrest/src/main/java/com/springrest/springrest/Dao/courseImlpl.java
@@ -1,30 +1,30 @@
-//package com.springrest.springrest.Dao;
-//
-//import com.springrest.springrest.Entity.Course;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.data.domain.Example;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.domain.Sort;
-//import org.springframework.data.repository.query.FluentQuery;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.List;
-//import java.util.Optional;
-//import java.util.function.Function;
-//
-//@Component
-//public class courseImlpl implements CommandLineRunner {
-//
-//
-//   @Autowired
-//  public CourseDao courseDao;
-//
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        Course course = null;
-//        courseDao.save(course);
-//    }
-//}
+package com.springrest.springrest.Dao;
+
+import com.springrest.springrest.Entity.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+@Component
+public class courseImlpl implements CommandLineRunner {
+
+
+   @Autowired
+  public CourseDao courseDao;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Course course = null;
+        courseDao.save(course);
+    }
+}
